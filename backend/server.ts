3bb09e00317b95ae56813d386695904f3aca5601
garyDav <gary.2810.dav@gmail.com>
2024-06-14 18:31:59 -0400

    add: frontend, reconfig eslint

--- backend/server.ts
+++ backend/server.ts
@@ -1,28 +1,60 @@
 import express from 'express'
 import cors from 'cors'
+import multer from 'multer'
+import csvToJson from 'convert-csv-to-json'
 
 const app = express()
 const port = process.env.PORT ?? 3000
 
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+
+let userData: Array<Record<string, string>> = []
+
 app.use(cors()) // Enable CORS
 
-app.post('/api/files', async (req, res) => {
+app.post('/api/files', upload.single('file'), async (req, res) => {
   // 1. Extract file from request
+  const { file } = req
   // 2. Validate that we have file
+  if (!file) return res.status(500).json({ message: 'File is require' }) // status 400 rec.
   // 3. Validate the mimetype (csv)
-  // 4. Transform el File (Buffer) to string
-  // 5. Transform string to CSV
+  if (file.mimetype !== 'text/csv') return res.status(500).json({ message: 'File must be CSV' })
+
+  let json: Array<Record<string, string>> = []
+
+  try {
+    // 4. Transform el File (Buffer) to string
+    const rawCsv = Buffer.from(file.buffer).toString('utf-8')
+    console.log(rawCsv)
+
+    // 5. Transform string (csv) to JSON
+    json = csvToJson.csvStringToJson(rawCsv)
+  } catch (error) {
+    return res.status(500).json({ message: 'Error parsing the file' })
+  }
   // 6. Save the JSON to db (or memory)
+  userData = json
   // 7. Return 200 with the message and the JSON
-  return res.status(200).json({ data: [], message: 'El archivo se cargó correctamente.' })
+  return res.status(200).json({ data: json, message: 'El archivo se cargó correctamente.' })
 })
 
 app.get('/api/users', async (req, res) => {
   // 1. Extract the query param `q` from the request
+  const { q } = req.query
+
   // 2. Validate that we have the query param
+  if (!q) return res.status(500).json({ message: 'Query param `q` is require' })
+  if (Array.isArray(q)) return res.status(500).json({ message: 'Query param `q` must be a string' })
+
   // 3. Filter the data from the db (or memory) with the query param
+  const search = q.toString().toLowerCase()
+  const filteredData = userData.filter(row => {
+    return Object.values(row).some(value => value.toLowerCase().includes(search))
+  })
+
   // 4. Return 200 with the filtered data
-  return res.status(200).json({ data: [] })
+  return res.status(200).json({ data: filteredData })
 })
 
 app.listen(port, () => {
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,25 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettierPlugin from 'eslint-plugin-prettier'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintReact from 'eslint-plugin-react'
+import eslintReactHooks from 'eslint-plugin-react-hooks'
+import eslintReactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
   {
     plugins: {
       '@typescript-eslint': tseslint.plugin,
+      react: eslintReact,
+      'react-hooks': eslintReactHooks,
+      'react-refresh': eslintReactRefresh,
       prettier: prettierPlugin,
     },
   },
   {
-    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
+    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.mjs'],
   },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     languageOptions: {
       globals: {
@@ -21,18 +29,22 @@ export default [
         ...globals.node,
         ...globals.es2020,
       },
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json'],
+      },
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     rules: {
       ...prettierPlugin.configs.recommended.rules,
       ...eslintConfigPrettier.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'prefer-const': 'error',
+      'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+      'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
       'max-lines': ['warn', { max: 124 }],
     },
   },
 ]
-
